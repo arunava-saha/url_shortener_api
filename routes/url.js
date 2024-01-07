e535e0bea64877d6774e62bf96e8785b19d1ad71
Arunava Saha <67873400+arunava-saha@users.noreply.github.com>
2024-01-07 08:31:15 -0800

    Update url.js

--- routes/url.js
+++ routes/url.js
@@ -5,6 +5,6 @@ const verify = require("../config/verify");
 const router = express.Router();
 
 router.post("/", verify, generateShortURL);
-router.get("/:shortId", verify, getUrl);
+router.get("/:shortId", getUrl);
 
 module.exports = router;
